5c3e4f1ca00f27bdd878d19c2e42d427327b81e6
Tom Webber <t.c.w.webber@gmail.com>
2025-02-01 16:26:39 +0100

    add: tests

---
+++ tests/file.rs
@@ -0,0 +1,15 @@
+use std::fs::{self, read_to_string};
+
+use html_parser::parse_html;
+
+#[test]
+fn test() {
+    let content = read_to_string("tests/data/index.html").unwrap();
+    let tree = parse_html(&content).unwrap_or_else(|err| panic!("{err}"));
+    let formatted = format!("{tree}");
+    fs::write(".out.html", formatted).expect("Permission denied: failed to write to directory.");
+    assert!(
+        format!("{tree}") == content,
+        "Error occurred. Output written in .out.html. Use diff to see the problem."
+    );
+}
